ed7817a6e2756780e9a411ac87eceddc84f696ce
robkle <springbokvlakte@yahoo.com>
2021-03-27 21:44:15 +0200

    leak fixed

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dthan <dthan@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 20:14:36 by ihwang            #+#    #+#             */
-/*   Updated: 2021/03/27 20:58:12 by rklein           ###   ########.fr       */
+/*   Updated: 2021/03/27 21:43:38 by rklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ static int	init_shell(char **envp)
 	if (init_job_control_stuff() == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	if (init_history_stuff() == EXIT_FAILURE)
-		return (EXIT_FAILURE);
+		exit(0);
 	init_signal_and_other_stuff();
 	return (EXIT_SUCCESS);
 }
